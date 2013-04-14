d02a170b89b522afc711968e7c5586c67d146f2f
theKunz <akstarcom777@yahoo.com>
2013-04-14 18:45:13 -0300

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,11 +1,13 @@
-
 import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Graphics;
 import java.awt.Color;
 import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
-public class Course extends Canvas{
+public class Course extends Canvas
+{
     
     double friction;
     double time;
@@ -14,7 +16,8 @@ public class Course extends Canvas{
     
     public Course()       
     {
-        frame = new JFrame("FRAME");
+        
+        frame = new JFrame("Minigolf Simulation");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,600);
         frame.setResizable(false);
@@ -23,13 +26,27 @@ public class Course extends Canvas{
         f.setBounds(center.x - 800 / 2, center.y - 600 / 2, 800,
         600);
         
+        frame.add(this);
         setVisible(true);
     }
-    
+    /**
+     * This paints the field, including the hole, onto the canvas.
+     * The dimensions and positions are hard coded for now, but
+     * we can make the parameters variables for dynamic course creation
+     * @param boll 
+     */
     public void paint(Graphics g)
     {
         super.paint(g);
-        
+        g.setColor(new Color(160, 120, 75));
+        g.fill3DRect(50, 340, 700, 20, true); //top boundary
+        g.fill3DRect(30, 340, 20, 200, true); //left boundary
+        g.fill3DRect(50, 520, 700, 20, true); //bottom boundary
+        g.fill3DRect(730, 340, 20, 200, true); //right boundary
+        g.setColor(new Color(0, 150, 75));
+        g.fillRect(50, 360, 680, 160); //grass
+        g.setColor(Color.BLACK);
+        g.fillOval(650, 425, 25, 25); // hole
     }
     
     /*public Ball getGolfBall()
