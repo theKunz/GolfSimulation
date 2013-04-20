71971e1b04d9ba37622c2168be1a7cfe5100f7c8
theKunz <akstarcom777@yahoo.com>
2013-04-20 16:13:32 -0300

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,78 +1,44 @@
-import java.awt.BorderLayout;
-import java.awt.Canvas;
-import java.awt.Graphics;
+
+package course;
+import java.awt.*;
 import java.awt.Color;
 import javax.swing.JFrame;
-import javax.swing.*;
-import java.awt.*;
 
+/**
+ *
+ * @author Aaron
+ */
 public class Course extends Canvas
 {
+
+
     
-    double friction;
-    double time;
-    
-    private JFrame frame;
-    
-    public Course()       
+    public Course()
     {
-        
-        frame = new JFrame("Minigolf Simulation");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,600);
-        frame.setResizable(false);
-        
-        Point center = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
-        f.setBounds(center.x - 800 / 2, center.y - 600 / 2, 800,
-        600);
-        
-        frame.add(this);
-        setVisible(true);
+        this.setBounds(30, 340, 740, 200);
     }
     /**
-     * This paints the field, including the hole, onto the canvas.
+     * This paints the field, including the hole, onto the canvas
      * The dimensions and positions are hard coded for now, but
-     * we can make the parameters variables for dynamic course creation
-     * @param boll 
+     * we can make the parameters variables for a dynamic course
+     * @param putt 
      */
-    public void paint(Graphics g)
+    @Override
+    public void paint(Graphics putt)
     {
-        super.paint(g);
-        g.setColor(new Color(160, 120, 75));
-        g.fill3DRect(50, 340, 700, 20, true); //top boundary
-        g.fill3DRect(30, 340, 20, 200, true); //left boundary
-        g.fill3DRect(50, 520, 700, 20, true); //bottom boundary
-        g.fill3DRect(730, 340, 20, 200, true); //right boundary
-        g.setColor(new Color(0, 150, 75));
-        g.fillRect(50, 360, 680, 160); //grass
-        g.setColor(Color.BLACK);
-        g.fillOval(650, 425, 25, 25); // hole
-    }
-    
-    /*public Ball getGolfBall()
-    {
+        super.paint(putt);
+        putt.setColor(new Color(160, 120, 75));
+        putt.fill3DRect(50, 340, 700, 20, true); //top boundary
+        putt.fill3DRect(30, 340, 20, 200, true); //left boundary
+        putt.fill3DRect(50, 520, 700, 20, true); //bottom boundary
+        putt.fill3DRect(730, 340, 20, 200, true); //right boundary
+        putt.setColor(new Color(0, 150, 75));
+        putt.fillRect(50, 360, 680, 160); //grass
+        putt.setColor(Color.BLACK);
+        putt.fillOval(650, 425, 25, 25); // hole
         
-    }*/
     
-    public double getFriction()
-    {
-        return this.friction;
     }
-    
-    public void setFriction(double frictiony)
-    {
-        this.friction = frictiony;
-    }
-    
-    public double getTime()
-    {
-        return this.time;
-    }
-    
-    public void setTime(double timey)
-    {
-        this.time = timey;
-    }
-    
-    
+            
+            
 }
