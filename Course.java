a2b1593cb367146c9191672f4a07201fabe46e3e
theKunz <akstarcom777@yahoo.com>
2013-04-26 17:03:08 -0300

    Update Course.java

--- Course.java
+++ Course.java
@@ -12,12 +12,16 @@ public class Course extends Canvas
     private double friction;
     private Ball ball;
     private double time;
+    private int length;
+    private int height;
 
     
     public Course()
     {
         this.setBounds(30, 340, 740, 200);
-        ball = new Ball();
+        //ball = new Ball();
+        length = 680; //680 default
+        height = 160; //160 default        
     }
     /**
      * This paints the field, including the hole, onto the canvas
@@ -30,20 +34,20 @@ public class Course extends Canvas
     {
         super.paint(putt);
         putt.setColor(new Color(160, 120, 75));
-        putt.fill3DRect(50, 340, 700, 20, true); //top boundary
-        putt.fill3DRect(30, 340, 20, 200, true); //left boundary
-        putt.fill3DRect(50, 520, 700, 20, true); //bottom boundary
-        putt.fill3DRect(730, 340, 20, 200, true); //right boundary
+        putt.fill3DRect(50, 340, length, 20, true); //top boundary
+        putt.fill3DRect(30, 340, 20, height + 40, true); //left boundary
+        putt.fill3DRect(50, 340 + height + 20, length, 20, true); //bottom boundary
+        putt.fill3DRect(30 + length + 20, 340, 20, height + 40, true); //right boundary
         putt.setColor(new Color(0, 150, 75));
-        putt.fillRect(50, 360, 680, 160); //grass
+        putt.fillRect(50, 360, length, height); //grass
         putt.setColor(Color.BLACK);
-        putt.fillOval(650, 425, 25, 25); // hole
+        putt.fillOval(length - 30, 345 + (height/2), 25, 25); // hole
         
         putt.setColor(Color.WHITE);
         
-        ball.move(time);
+        //ball.move(time);
         
-        putt.fillOval((int)ball.getCurrentX(), (int)ball.getCurrentY(), 25, 25); // ball
+        //putt.fillOval((int)ball.getCurrentX(), (int)ball.getCurrentY(), 25, 25); // ball
         
     
     }
@@ -84,6 +88,34 @@ public class Course extends Canvas
     public void setTime(double t)
     {
         time = t;
+    }
+    /**
+     * returns the width
+     */
+    public int getLength()
+    {
+        return length;
+    }
+    /**
+     * sets the width
+     */
+    public void setLength(int longs)
+    {
+        length = longs;
+    }
+    /**
+     * returns the height
+     */
+    public int getHeight()
+    {
+        return height;
+    }
+    /**
+     * sets the width
+     */
+    public void setHeight(int high)
+    {
+        height = high;
     }
     
     
